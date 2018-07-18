0e96c5649dd8724ca4d7eea7f45d26bfd1a13cc5
Kristina Lim <kristinalim.ph@gmail.com>
2018-07-18 18:54:49 +0800

    Add empty module for helpers for controller specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -62,6 +62,7 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Test::ControllerHelpers, type: :controller
 
   config.before(:suite) do
     FactoryBot.find_definitions
